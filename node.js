e02bee744c995967a49771725d0553fb8023efeb
Luca Marchal <lucamarchal@gmail.com>
2015-10-02 18:57:24 +0100

    Fixing

--- node.js
+++ node.js
@@ -1,15 +1,17 @@
-var http = require('http');
 var express = require('express'); 
 var app = express();
 var bodyParser = require('body-parser');
+
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json('application/json'));
+
 app.use(express.static(__dirname));
 
-app.get('/', function(res, req){
+app.get('/', function(req, res){
 	res.sendfile('index.html');
 });
-app.post('/houses', function(res, req){
-	var location = req.body;
+app.post('/houses', function(req, res){
+	var location = req.body.location;
 	console.log("From Client Post request: location = " + location);
 });
 
