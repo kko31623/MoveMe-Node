6db42626f7da5af81b2ca1036504ee3af01be34f
Luca Marchal <lucamarchal@gmail.com>
2015-10-02 19:34:15 +0100

    additions

--- node.js
+++ node.js
@@ -7,12 +7,18 @@ app.use(bodyParser.json('application/json'));
 
 app.use(express.static(__dirname));
 
+var request = require('request');
+
 app.get('/', function(req, res){
-	res.sendfile('index.html');
+  res.sendfile('index.html');
 });
+
 app.post('/houses', function(req, res){
-	var location = req.body.location;
-	console.log("From Client Post request: location = " + location);
+  var location = req.body.location;
+  var url = 'http://api.zoopla.co.uk/api/v1/property_listings.json?area=' + location + '&api_key=c88442waawanw44ygzem2ee2';
+  request(url, function (error, response, body) {
+    res.send(body);
+  })
 });
 
  var port = 3030;
